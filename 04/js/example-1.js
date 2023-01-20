5b2a26558774ba9bd0ae1983ca71a82a220dc74e
yubeenso <youbin0105@gmail.com>
2023-01-20 23:07:05 +0900

    04-1

---
+++ 04/js/example-1.js
@@ -0,0 +1,15 @@
+function mul(a, b) {
+  console.log(a * b);
+}
+
+num1 = parseInt(prompt("첫번째 수를 입력하세요."));
+num2 = parseInt(prompt("두번째 수를 입력하세요."));
+
+mul(num1, num2);
+
+// 10, 20을 받아 곱한 결괏값을 콘솔에 나타내는 함수
+// function multiple(a, b) {
+//   return a * b;
+// }
+
+// console.log(`두 수를 곱한 결과는 ${multiple(10, 20)} 입니다.`);
