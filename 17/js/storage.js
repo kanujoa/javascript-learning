14fc61186a510eede649f7efb99d8a00548d30c8
yubeenso <youbin0105@gmail.com>
2023-02-26 23:02:47 +0900

    17-2

--- 17/js/storage.js
+++ 17/js/storage.js
@@ -1,24 +1,25 @@
+// 1. 로컬 스토리지에 저장
 let students = ["Kim", "Lee", "Park"];
-// 스토리지에 students 키로 배열을 저장
-localStorage.setItem("students", JSON.stringify(students));
+// localstorage에 키 "students"와 값(JSON 형식으로 바꾼 students) 저장
+localStorage.setItem("students", JSON.stringify(students));   
 
-// 로컬 스토리지에서 가져온 후 추가하고 저장
-let localData
-if(localStorage.getItem("students") === null) {     // 스토리지에 students 키가 있는지 확인
-  localData = [];
-} else {
-  localData = JSON.parse(localStorage.getItem("students"));     // 스토리지의 값을 localData로 저장
+// 2. 로컬 스토리지에 값 추가 (로컬 스토리지에서 값을 가져온 후 값 추가한 다음 다시 저장해야 함.)
+let localData;     // localStorage에서 가져올 데이터를 저장할 변수 생성
+if (localStorage.getItem("students") === null) {     // 스토리지에 students 키가 있는지 확인하고 없으면 
+  localData = [];     // 가져올 데이터 없음. (빈 리스트 저장)
+} else {     // storage에 students 키가 있으면 
+  localData = JSON.parse(localStorage.getItem("students"));     // localStorage에서 students 키에 대한 값을 가져와 배열 형태로 바꾸고 그 값 저장
 }
+localData.push("Choi");      // 값 Choi 추가
+localStorage.setItem("students", JSON.stringify(localData));     // localStorage에 새롭게 추가된 값이 있는 리스트를 JSON 형식으로 바꾸어 저장하기 
+// (추가되는 것이 아니라 기존 값을 새로운 값으로 업데이트하는 것임.)
 
-localData.push("Choi");     // localData에 Choi를 추가
-localStorage.setItem("students", JSON.stringify(localData))     // 스토리지에 localData를 저장
-console.log(`추가 후 students : ${localData}`);     // 스토리지에 저장된 값을 표시
-
-// 로컬 스토리지에서 특정 값을 삭제
+// 3. 로컬 스토리지에서 특정 값 삭제하기 
+// (위의 localData와 현재 localStorage에 있는 값이 같으므로 localData 이용, 인덱스를 찾아 그 부분만 삭제한다.)
 const indexOfValue = localData.indexOf("Lee");     // 인덱스 탐색
-localData.splice(indexOfValue, 1);     // 인덱스에 해당하는 값부터 1개를 삭제
-localStorage.setItem("students", JSON.stringify(localData));
-console.log(`삭제 후 students : ${localData}`);
+localData.splice(indexOfValue, 1);     // 인덱스에 해당하는 값 1개만 삭제
+localStorage.setItem("students", JSON.stringify(localData));     // localStorage에 다시 저장
 
-// 로컬 스토리지에서 특정 키 삭제
-localStorage.removeItem("students");     // students 키에 해당하는 키와 값만 삭제함. (clear 메서드는 로컬 스토리지 전체를 삭제함.)
+// 4. 로컬 스토리지에서 특정 키 삭제
+localStorage.removeItem("students");
+// clear() 메서드는 removeItem 메서드와 다르게 로컬 스토리지 전체를 지운다.
--- 17/js/todo.js
+++ 17/js/todo.js
@@ -1,86 +1,38 @@
-const form = document.querySelector("form");
-const ul = document.querySelector("ul");
+// 웹 요소 가져오기
+const todoInput = document.querySelector("input");     // 사용자 입력
+const form = document.querySelector("form");     // 폼
+const todoList = document.querySelector("#todo-list");     // 할 일 목록
 
-function addToDo(event) {
-  event.preventDefault();
+function addTodo(e) {
+  e.preventDefault();     // submit 이벤트의 기본 동작 취소
   
-  const createLi = document.createElement("li");
-  const li = ul.appendChild(createLi);
+  // 새로운 내용(HTML) 추가
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newDiv.appendChild(newTodo);
   
-  const createP = document.createElement("p");
-  const createCompleteButton = document.createElement("button");
-  const createRemoveButton = document.createElement("button");
-  const p = li.appendChild(createP);
-  const completeButton = li.appendChild(createCompleteButton);
-  const removeButton = li.appendChild(createRemoveButton);
-  completeButton.innerText = "✅";
-  removeButton.innerText = "❌";
-  completeButton.className = "complete_button";     // classList.add("classname")으로도 작성 가능
-  removeButton.className = "remove_button";
+  // 사용자가 입력란에 입력한 내용(todo 내용)을 localStorage에 저장한다. (todoInput.value를 ""로 바꾸기 전에 작성해야 정상적으로 작동!)
+  saveToLocal(todoInput.value);
 
-  const toDoInput = document.querySelector("input"); 
-  p.innerText = toDoInput.value;
-  saveToDo(toDoInput.value);      // local storage에 저장
-  toDoInput.value = "";     // b = a.value;라고 했을 때 b = ""; 라고 적어주면 제대로 작동 X (옆처럼 a.value = ""; 이렇게 직접적으로 적어주어야 함.)
+  // 내용의 오른쪽에 버튼을 추가
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "완료";
+  completeButton.classList.add("complete");
+  newDiv.appendChild(completeButton);
+
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "삭제";
+  removeButton.classList.add("remove");
+  newDiv.appendChild(removeButton);
+
+  // newDiv <ul>에 추가 후 입력란 공란으로 만들기
+  todoList.appendChild(newDiv);
+  todoInput.value = "";
 }
 
-function saveToDo(todo) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {     // storage에 todos 키가 있는지 없는지 확인하기 (따옴표 꼭 작성하기! 없으면 빈 리스트 생성, 있으면 그에 대한 값을 그대로 가져옴. 가져온 후에 아래의 코드를 통해 local storage에 새 todo 추가)
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-  todos.push(todo);     // 새 todo todos 리스트에 추가하기 (push 메서드이므로 인자가 들어가야 하는 것을 기억!)
-  localStorage.setItem("todos", JSON.stringify(todos));     // local storage에 수정된 todos 반영하기
-}
-
-function getLocal() {
-  let todos;
-  if (localStorage.getItem('todos') === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));     // 스토리지에서 todos 값을 가져옴.
-  }
-
-  todos.forEach(function(todo) {     // todo 요소마다 반복하기기
-  const createLi = document.createElement("li");
-  const li = ul.appendChild(createLi);
-   
-  const createP = document.createElement("p");
-  const createCompleteButton = document.createElement("button");
-  const createRemoveButton = document.createElement("button");
-  const p = li.appendChild(createP);
-  const completeButton = li.appendChild(createCompleteButton);
-  const removeButton = li.appendChild(createRemoveButton);
-  p.innerText = todo;
-  completeButton.innerText = "✅";
-  removeButton.innerText = "❌";
-  completeButton.className = "complete_button";     
-  removeButton.className = "remove_button"; 
-
-  const toDoInput = document.querySelector("input"); 
-  toDoInput.value = "";     
-  });
-
-  let completed;     // 새로고침 이후에도 css(line-through) 유지를 위한 작업
-  if (localStorage.getItem("completed") === null) {
-    completed = [];
-  } else {
-    completed = JSON.parse(localStorage.getItem("completed"));
-  }
-
-  const p = document.querySelectorAll("p");
-  completed.forEach(function(completedtodo){
-    for (let index = 0; index < p.length; index++) {     // for (변수 정의; 조건; 증감식) 이 순서인 것을 기억하기!!
-      if (p[index].innerText === completedtodo) {
-        p[index].classList.add("completed");
-      }
-    }
-  })
-}
-
-function removeLocal(todo) {     // 여기에 인자로 들어가는 todo는 <li> 묶음이다.
+function saveToLocal(todo) {     // localStorage에 아무것도 안뜰 때는 localStorage.clear()를 하고 다시 실행해 보기
   let todos;
   if (localStorage.getItem("todos") === null) {
     todos = [];
@@ -88,43 +40,9 @@ function removeLocal(todo) {     // 여기에 인자로 들어가는 todo는 <li
     todos = JSON.parse(localStorage.getItem("todos"));
   }
 
-  const todoIndex = todos.indexOf(todo.children[0].innerText);     // 삭제할 할 일의 idndex (todos 리스트에서 todo(<li> 묶음) text가 몇 번째 index인지를 알아야 한다.)
-  todos.splice(todoIndex, 1);     // index 번째 요소를 삭제
+  todos.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
-
-  let completed;
-  if (localStorage.getItem("completed") === null) {
-    completed = [];
-  } else {
-    completed = JSON.parse(localStorage.getItem("completed"));
-  }
-
-  const completedIndex = completed.indexOf(todo.children[0].innerText);
-  completed.splice(completedIndex, 1);
-  localStorage.setItem("completed", JSON.stringify(completed)); 
 }
 
-function manageToDo(event) {
-  const whichButton = event.target.classList[0];     // 클릭한 부분의 클래스명을 가져옴.
-  if (whichButton === 'complete_button') {     // 클릭한 것의 class가 complete_button이면 (완료 버튼을 클릭하면)
-    const toDoContainer = event.target.parentElement;     // button의 부모 태그인 li를 정의
-    toDoContainer.children[0].classList.toggle("completed");     // li의 자식 요소 중 첫번째(p)에 .completed 스타일을 토글함.
-
-    const todosText = document.querySelectorAll("p");     // line-through 스타일이 적용된 것들을 localStorage에 저장함. (새로고침 이후에도 css 유지를 위한 작업)
-    let completed = [];
-    todosText.forEach(function (todoText) {
-    if (todoText.classList[0] === "completed") {
-      completed.push(todoText.innerText);
-      localStorage.setItem("completed", JSON.stringify(completed));
-    }
-    }); 
-  } else if(whichButton === "remove_button") {     // 클릭한 부분의 class가 delete_button이면 (삭제 버튼을 클릭하면)
-    const toDoContainer = event.target.parentElement;
-    removeLocal(toDoContainer);     // 화면에서도 지우고 localstorage에서도 삭제함. (removeLocal 함수의 인자로 toDoContainer를 넣음으로써)
-    toDoContainer.remove();
-  }
-}
-
-document.addEventListener("DOMContentLoaded", getLocal);
-form.addEventListener("submit", addToDo);
-ul.addEventListener("click", manageToDo);
+// 이벤트 처리
+form.addEventListener("submit", addTodo);
