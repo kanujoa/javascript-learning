748b2818dab8c0cf77869ac900398c0b57f3744e
yubeenso <youbin0105@gmail.com>
2023-04-21 22:44:01 +0900

    09_review-3

---
+++ 09 review/proto.js
@@ -0,0 +1,40 @@
+// const myPet = {
+//   name: 'Kanu',
+//   age: 7,
+//   eat: ['fruit', 'grain', 'feed'],
+//   language: false,
+//   speak: language ? `${this.name} can speak.` : `${this.name} can't speak.`,
+// };
+
+// 생성자 함수로 정의하기
+const myPet = function (name, age, eat, language) {
+  this.name = name;
+  this.age = age;
+  this.eat = eat;
+  this.language = language;
+
+  this.canSpeak = function () {
+    const speak = language ? `${this.name} can speak.` : `${this.name} can't speak.`;
+    return speak;
+  };
+};
+
+// 클래스로 정의하기
+// class myPet {
+//   constructor(name, age, eat, language) {
+//     this.name = name;
+//     this.age = age;
+//     this.eat = eat;
+//     this.language = language;
+//   }
+
+//   canSpeak() {
+//     const speak = language ? `${this.name} can speak.` : `${this.name} can't speak.`;
+//     return speak;
+//   }
+// }
+
+const pet1 = new myPet('Kanu', 7, ['fruit', 'grain', 'feed'], true);
+console.log(pet1);
+console.log(pet1.eat);
+console.log(pet1.canSpeak());
